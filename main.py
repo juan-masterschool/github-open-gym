6db11947bd76db830cb6197958159f9c6068c2e5
Juan <juan@faculty.masterschool.com>
2026-04-15 10:34:20 +0200

    First commit for frontend for windows

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('Everyone')
     print_hi('No one')
+    print_hi('Frontend for Windows')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
