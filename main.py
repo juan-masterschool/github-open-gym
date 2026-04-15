3c142f03453db222dbddf483095dd7f8f7cae287
juan-masterschool <167711474+juan-masterschool@users.noreply.github.com>
2026-04-15 10:12:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('Everyone')
+    print_hi('No one')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
